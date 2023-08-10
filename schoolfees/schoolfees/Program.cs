4a6f7b76c135869399dd514167887fbbe059b7da
victor mbugua <57389085+blackswanalpha@users.noreply.github.com>
2023-08-10 22:36:49 +0300

    Add files via upload
    learn c#

---
+++ schoolfees/schoolfees/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolfees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float P, T;
+            T = 1;
+            float R, SI;
+            Console.Write("Enter Amount :");
+            P = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Rate :");
+            R = Convert.ToSingle(Console.ReadLine());
+            SI = 0;
+            
+            
+
+
+            while (SI >= (2*P))
+            {
+                T = T + 1;
+                SI = (P * R)+ P ;
+                P = SI;
+            Console.WriteLine("Simple Interest is :"+ SI);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
