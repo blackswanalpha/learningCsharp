5d62abbd3dab16fecef0751e9341d3b63a863929
victor mbugua <57389085+blackswanalpha@users.noreply.github.com>
2023-08-10 22:59:45 +0300

    Add files via upload
    learning C#

---
+++ BMIrevise/BMIrevise/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIrevise
+{
+    class Person
+    {
+        String name;
+        float height;
+        float weight;
+        DateTime dob = new DateTime();
+    }
+}
---
+++ BMIrevise/BMIrevise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIrevise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float POUND_KILOGRAM = (float)0.45359237;
+            const float INCH_METRE = (float)0.0254;
+
+            float weightPound, heightInches, weightKilogram, heightMetres, BMI;
+            BMI = weightKilogram / (float)Math.Pow(heightMetres, 2);
+
+            if (BMI >= 30)
+                Console.WriteLine("Obese");
+            else if (BMI >= 25 && BMI < 30)
+                Console.WriteLine("Overweight");
+            else if (BMI >= 18.5 && BMI < 25)
+                Console.WriteLine("Normal");
+            else
+                Console.WriteLine("Underweight");
+
+            Console.ReadKey();
+        }
+    }
+}
